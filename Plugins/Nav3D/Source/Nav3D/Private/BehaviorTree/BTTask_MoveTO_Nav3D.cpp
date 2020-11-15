a82ae5c47e675c79fd887c67a56ac9ff54afb3ab
yhyu13 <yohan680919@gmail.com>
2020-11-14 21:48:57 -0800

    Checkpoint : convert to c++ project - 2

---
+++ Plugins/Nav3D/Source/Nav3D/Private/BehaviorTree/BTTask_MoveTO_Nav3D.cpp
@@ -0,0 +1,20 @@
+/* Start Header -------------------------------------------------------
+Copyright (C) 2020 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+Language: c++ 14
+Platform: Windows 10 (X64)
+Project: CS380
+Author: Hang Yu (hang.yu@digipen.edu | 60001119)
+Creation date: 11/14/2020
+- End Header ----------------------------*/
+
+#include "BehaviorTree/BTTask_MoveTo_Nav3D.h"
+
+BTTask_MoveTo_Nav3D::BTTask_MoveTo_Nav3D()
+{
+}
+
+BTTask_MoveTo_Nav3D::~BTTask_MoveTo_Nav3D()
+{
+}
---
+++ Plugins/Nav3D/Source/Nav3D/Private/Nav3DBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Nav3DBPLibrary.h"
+#include "Nav3D.h"
+
+UNav3DBPLibrary::UNav3DBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UNav3DBPLibrary::Nav3DSampleFunction(float Param)
+{
+	return -1;
+}
+
