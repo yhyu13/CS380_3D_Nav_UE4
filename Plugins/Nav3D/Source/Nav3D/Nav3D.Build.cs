364de30baf89487dc725948224cea16e5972c073
yhyu13 <yohan680919@gmail.com>
2020-11-29 15:01:42 -0800

    Success in porting DoN code into Nav3D

--- Plugins/Nav3D/Source/Nav3D/Nav3D.Build.cs
+++ Plugins/Nav3D/Source/Nav3D/Nav3D.Build.cs
@@ -1,53 +1,51 @@
-// Some copyright should be here...
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
-using UnrealBuildTool;
-
-public class Nav3D : ModuleRules
+namespace UnrealBuildTool.Rules
 {
-	public Nav3D(ReadOnlyTargetRules Target) : base(Target)
+	public class Nav3D : ModuleRules
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+		public Nav3D(ReadOnlyTargetRules Target) : base(Target)
+		{
+            PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+            bEnableShadowVariableWarnings = false;
+
+            PublicIncludePaths.AddRange(
+				new string[] {
+					// ... add public include paths required here ...
+				}
+				);
+
+			PrivateIncludePaths.AddRange(
+				new string[] {
+					// ... add other private include paths required here ...
+                }
+				);
+
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+					// ... add other public dependencies that you statically link with here ...                    
+                    "AIModule",
+                    "GameplayTasks",
+                    "Engine",
+                }
+				);
+
+            PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					// ... add private dependencies that you statically link with here ...
+				}
+				);
+
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[]
+				{
+					// ... add any modules that your module loads dynamically here ...
+				}
+				);
+		}
 	}
 }
